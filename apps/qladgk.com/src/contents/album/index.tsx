fa57f325143bb44109c43c49d9964fe24bcfbc2b
qlAD <qlad.adgk.1024@gmail.com>
2024-10-21 21:06:05 +0800

    取消相册页的查看大图模式

--- apps/qladgk.com/src/contents/album/index.tsx
+++ apps/qladgk.com/src/contents/album/index.tsx
@@ -1,4 +1,3 @@
-import { Fancybox } from '@fancyapps/ui';
 import clsx from 'clsx';
 import Image from 'next/image';
 
@@ -6,57 +5,19 @@ import BlurFade from '@/components/magicui/BlurFade';
 
 import externalImages from './ExternalImages';
 
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
-
 function BlurFadeDemo() {
-  const handleImageClick = (imageUrls) => {
-    Fancybox.show(
-      imageUrls.map((src) => ({
-        src,
-        opts: {
-          caption: '相册图片',
-        },
-      })),
-      {
-        Toolbar: {
-          display: {
-            left: ['infobar'],
-            middle: ['zoomIn', 'zoomOut'],
-            right: ['download', 'close'],
-          },
-        },
-      }
-    );
-  };
-
-  const handleKeyDown = (event, imageUrls) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault();
-      handleImageClick(imageUrls);
-    }
-  };
-
   return (
     <section id="photos">
       <div className="columns-2 gap-4 sm:columns-3">
         {externalImages.map((imageUrl, idx) => (
           <BlurFade key={imageUrl} delay={0.25 + idx * 0.05} inView>
-            <div
-              className="cursor-pointer"
-              onClick={() => handleImageClick(externalImages)}
-              onKeyDown={(event) => handleKeyDown(event, externalImages)}
-              role="button"
-              tabIndex={0}
-              aria-label={`查看相册图片 ${idx + 1}`}
-            >
-              <Image
-                className="size-full mb-4 rounded-lg object-contain"
-                src={imageUrl}
-                width={800}
-                height={600}
-                alt={`Album image ${idx + 1}`}
-              />
-            </div>
+            <Image
+              className="size-full mb-4 rounded-lg object-contain"
+              src={imageUrl}
+              width={800}
+              height={600}
+              alt={`Album image ${idx + 1}`}
+            />
           </BlurFade>
         ))}
       </div>
